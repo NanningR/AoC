bac8a9f5374912b4ee21f7b389018730e2900b01
Nanning van Rossen <nanning.vanrossen@vitecsoftware.com>
2025-12-13 19:32:21 +0100

    Add base template

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n = 0;
+    ll answer = 0;
+
+    cout << answer << endl;
+
+    return 0;
+}
